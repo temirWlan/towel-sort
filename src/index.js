f8e93efad68e72105d8170d4bfbd9739584af948
Temirlan <temirlan.balguzhin@bk.ru>
2020-10-08 16:27:18 +0600

    feat: add function that open arrays in array. complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix) { 
+	if (!matrix) return [];
+
+	const array = [];
+	
+	for (let i = 0; i < matrix.length; i++) {
+		if (i % 2 !== 0) {
+			array.push(...matrix[i].reverse());
+		} else {
+			array.push(...matrix[i]);
+		}
+	}
+
+	return array;
 }
+
+
+
+
